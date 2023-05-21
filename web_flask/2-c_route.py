9b0b0254ffbedc85617837f0554a0b1f54f257e3
nerbrain <malikmox@gmail.com>
2023-05-21 21:28:16 +0000

    task 1

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,6 +4,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ / route """
@@ -15,10 +16,12 @@ def hbnd():
     """/hbnb route"""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """ /c/<text> route """
     return 'C %s' % text.replace('_', ' ')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
