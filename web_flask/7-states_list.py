e0238de41407957a76a3ef1e18e4a05d7525fd60
nerbrain <malikmox@gmail.com>
2023-05-21 22:43:09 +0000

    pep8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -21,5 +21,6 @@ def display_states():
     states.sort(key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
